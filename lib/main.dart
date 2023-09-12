21a5e02042136cf24a8aef9e5185159c919ab689
Sejal <sejaljain2044@gmail.com>
2023-09-12 13:02:34 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key?key}):super(key:key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var wtController = TextEditingController();
+  var ftController = TextEditingController();
+  var inController = TextEditingController();
+  var result = "";
+  var bgColor = Colors.indigo.shade200;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('yourBMI'),
+      ),
+      body: Container(
+        color: bgColor,
+        child: Center(
+          child: Container(
+            width: 300,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                Text('BMI', style: TextStyle(
+                  fontSize: 34,fontWeight: FontWeight.w700
+                ),),
+
+                SizedBox(height:21,),
+
+                TextField(
+                  controller: wtController,
+                  decoration:InputDecoration(
+                    label:Text('Enter your weight (in kgs)'),
+                     prefixIcon:Icon(Icons.line_weight),
+                ),
+                  keyboardType: TextInputType.number,
+                ),
+
+                SizedBox(height:11,),
+
+                TextField(
+                  controller: ftController,
+                  decoration:InputDecoration(
+                      label:Text('Enter your Height (in feet)'),
+                      prefixIcon:Icon(Icons.height),
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+
+                SizedBox(height:11,),
+                TextField(
+                  controller: inController,
+                  decoration:InputDecoration(
+                      label:Text('Enter your Height (in inches)'),
+                      prefixIcon:Icon(Icons.height),
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+
+                SizedBox(height:16,),
+                ElevatedButton(onPressed:(){
+
+                  var wt = wtController.text.toString();
+                  var ft = ftController.text.toString();
+                  var inch = inController.text.toString();
+
+                  if(wt!="" && ft!="" && inch!=""){
+                    //BMI calculation
+
+                    var iwt = int.parse(wt);
+                    var ift = int.parse(ft);
+                    var iInch = int.parse(inch);
+
+                    var tInch=(ift*12) + iInch;
+
+                    var tcm = tInch*2.54;
+
+                    var tM = tcm/100;
+
+                    var bmi = iwt/(tM*tM);
+
+                    var msg = "";
+                    if(bmi>25){
+                     msg = "You're OverWeight!!";
+                     bgColor = Colors.orange.shade200;
+                    }
+                    else if(bmi<18){
+                      msg="You're UnderWeight!!";
+                      bgColor = Colors.red.shade200;
+                    }
+                    else{
+                       msg = "You are healthy!!";
+                       bgColor = Colors.green.shade200;
+                    }
+
+                    setState(() {
+                      result="$msg \n Your BMI is : ${bmi.toStringAsFixed(4)}";
+                    });
+
+
+
+                  }
+                  else{
+                     setState(() {
+                       result = "Please fill all the required blanks!! ";
+                     });
+                  }
+
+
+                }, child:Text('Calculate')),
+
+                SizedBox(height: 11,),
+
+                Text(result, style: TextStyle(fontSize: 19),)
+              ],
+            ),
+          ),
+        ),
+      )
+
+    );
+  }
+}
